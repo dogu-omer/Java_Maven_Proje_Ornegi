707f37859a68e8168edd7f6a1bea6d736be1bf1b
omerdogu <omerdogu@Yasemin>
2024-11-24 01:24:49 +0300

    First commit

---
+++ src/Controller/Control.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import com.test.database.Repository;
+
+public class Control {
+	
+	public static void main(String[] args) {
+		
+		Repository repo=new Repository();
+		repo.insert("Yasemin");
+		repo.delete("Sandal");
+	}
+	
+
+}
