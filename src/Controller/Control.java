749577f0d4e5f648a5778b6c0313a0da20453df4
omerdogu <omerdogu@Yasemin>
2024-11-24 16:37:23 +0300

    update

--- src/Controller/Control.java
+++ src/Controller/Control.java
@@ -3,13 +3,14 @@ package Controller;
 import com.test.database.Repository;
 
 public class Control {
-	
+	 
 	public static void main(String[] args) {
 		
 		Repository repo=new Repository();
 		repo.insert("Yasemin");
 		repo.delete("Sandal");
+		//repo.update("ömer doğu");
 	}
 	
 
-}
+} 
